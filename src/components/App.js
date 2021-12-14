fb13790f04e11900ee44c8d55214d86fb7d84fb2
Evaldas <prevaldas@yahoo.com>
2021-12-14 09:59:49 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Goal from "./Goal";
+import Footer from "./Footer";
+import TimeOfDay from "./TimeOfDay";
+import DateAndYear from "./DateAndYear";
+import Time from "./Time";
+
+
+function App() {
+    return <div>
+    <DateAndYear />
+    <TimeOfDay />
+    <Time />
+    <Goal />
+    <Footer />
+    </div>
+}
+export default App;
